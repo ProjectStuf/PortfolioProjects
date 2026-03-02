c122e5d35bae14ba57cd05a0e8bc119f9714b5ef
Kyle Lumbo <Kyleadrian47lumbo@gmail.com>
2026-03-01 21:56:35 -0600

    Update Hotel Revenue Dataset Project.sql

--- Hotel Revenue Dataset Project.sql
+++ Hotel Revenue Dataset Project.sql
@@ -8,6 +8,18 @@
 */
 
 
+/* practice stakeholder questions
+"Is our hotel revenue growing by year"
+We have 2 hotel types so it would be good to segment revenue by hotel type
+
+"Should we increase our parking lot size?"
+We want to understand if there is a trend in guests with personal cars
+
+"What trends can we see in the data?"
+Focus on average daily rate and guests to explore seasonality
+*/
+
+
 /* This section is to join all the tables into one to create a full sorted dataset containing all important 
 information in order to import the dataset into Power BI to create a visual dashboard showing everything for the stakeholders */
 
@@ -23,19 +35,6 @@ left join dbo.market_segment$
 on hotels.market_segment = market_segment$.market_segment 
 left join dbo.meal_cost$
 on meal_cost$.meal = hotels.meal
-
-
-
-/* practice stakeholder questions
-"Is our hotel revenue growing by year"
-We have 2 hotel types so it would be good to segment revenue by hotel type
-
-"Should we increase our parking lot size?"
-We want to understand if there is a trend in guests with personal cars
-
-"What trends can we see in the data?"
-Focus on average daily rate and guests to explore seasonality
-*/
 
 
 
